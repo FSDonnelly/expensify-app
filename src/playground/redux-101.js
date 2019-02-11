b7b8dbd26ddfd1b9f4aabc589dba14fa58c44a52
FSDonnelly <donnelly_frank@yahoo.com>
2019-02-10 18:14:29 -0800

    using spread operators and reducers in redux-expensify.js

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -27,7 +27,12 @@ const setCount = ({ count }) => ({
     count
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+// ------------ Reducers ------------
+// 1. Reducers are pure functions
+// 2. Never change state or action
+
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -48,7 +53,9 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
