0fab010157d05a7744459c4b3a87b1aec182ad80
FSDonnelly <donnelly_frank@yahoo.com>
2019-02-13 23:57:09 -0800

    installed enzyme and created snapshot of Header component test

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../../components/header';
+
+test('should render Header correctly', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+
