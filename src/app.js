0ed0ecab915b5ea1ef21acff15980cb7b48c9782
FSDonnelly <donnelly_frank@yahoo.com>
2019-02-14 22:41:52 -0800

    created production build for webpack

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { addExpense } from './actions/expenses';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import getVisibleExpenses from './selectors/expenses';
+import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
