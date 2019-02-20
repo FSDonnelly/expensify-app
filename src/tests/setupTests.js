d969c5f46304d53da79e53aae4cd8a7265abe0d5
FSDonnelly <donnelly_frank@yahoo.com>
2019-02-20 15:28:01 -0800

    updated webpack

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -2,7 +2,7 @@ import DotEnv from 'dotenv';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-DotEnv.config({ path: `.env.test` });
+DotEnv.config({ path: '.env.test' });
 
 Enzyme.configure({
     adapter: new Adapter()
