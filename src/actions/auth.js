157fc002014ea3554a6e6ecdb12b2726fed0c8c4
FSDonnelly <donnelly_frank@yahoo.com>
2019-02-20 20:32:49 -0800

    login and logout buttons function and direct user to correct page

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,3 +7,9 @@ export const startLogin = () => {
     return firebase.auth().signInWithPopup(googleAuthProvider);
    };
 };
+
+export const startLogout = () => {
+   return () => {
+      return firebase.auth().signOut();
+   };
+};
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
@@ -8,9 +9,10 @@ import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
 
+export const history = createHistory();
 
 const AppRouter = () => (
-<BrowserRouter>
+<Router history={history}>
         <div>
         <Header />
         <Switch>
@@ -41,7 +43,7 @@ const AppRouter = () => (
         </Switch>
         </div>
         
-    </BrowserRouter>
+    </Router>
 );
 
 export default AppRouter;
